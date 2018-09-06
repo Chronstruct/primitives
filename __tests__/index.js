5a3f989eb25950409c1fd253db5d40d4667f3303
Kyle Poole <2kylepoole@gmail.com>
2018-09-05 23:19:25 -0700

    remove unused babelrc

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,8 +4,5 @@ const tester = require('babel-plugin-tester');
 tester({
   plugin: require('../src/index'),
   pluginName: 'chronstruct-primitives',
-//   babelOptions: {
-//     babelrc: true,
-//   },
   fixtures: path.join(__dirname, '..', '__fixtures__'),
 });
